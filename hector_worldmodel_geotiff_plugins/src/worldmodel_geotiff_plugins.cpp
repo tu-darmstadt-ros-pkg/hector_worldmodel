6eb56c1292fc864e77bb66ae2f2a7a48a337cf96
Johannes Meyer <meyer@fsr.tu-darmstadt.de>
2013-09-19 18:58:55 +0200

    hector_worldmodel_geotiff_plugins: reverted hector_geotiff header renaming to make the package compile with hector_slam-0.2.4

--- hector_worldmodel_geotiff_plugins/src/worldmodel_geotiff_plugins.cpp
+++ hector_worldmodel_geotiff_plugins/src/worldmodel_geotiff_plugins.cpp
@@ -26,8 +26,8 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //=================================================================================================
 
-#include <hector_geotiff/map_writer_interface.h>
-#include <hector_geotiff/map_writer_plugin_interface.h>
+#include <geotiff_writer/map_writer_interface.h>
+#include <geotiff_writer/map_writer_plugin_interface.h>
 
 #include <ros/ros.h>
 #include <hector_worldmodel_msgs/GetObjectModel.h>
