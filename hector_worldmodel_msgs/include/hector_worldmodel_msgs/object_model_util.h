22c9f8a6bfdde28d0db8abc1b200d8f378b69a3c
Christian Rose <christian.j.rose@gmx.de>
2015-12-10 16:24:45 +0100

    Util function to get object by object_id via iteration

---
+++ hector_worldmodel_msgs/include/hector_worldmodel_msgs/object_model_util.h
@@ -0,0 +1,18 @@
+#ifndef OBJECT_MODEL_H
+#define OBJECT_MODEL_H
+
+hector_worldmodel_msgs::Object* getObjectByID(hector_worldmodel_msgs::ObjectModel &object_model, std::string object_id){
+
+    // [object_id - 1] seems brittle
+    for(std::vector<hector_worldmodel_msgs::Object>::iterator it = object_model.objects.begin(); it != object_model.objects.end(); ++it) {
+        /* std::cout << *it; ... */
+        std::string object_name = it->info.name;
+        if (object_name.find(object_id) != std::string::npos) {
+            ROS_INFO("Object selected for id %s: %s", object_id.c_str(), object_name.c_str());
+            return &(*it);
+        }
+    }
+    return NULL;
+}
+
+#endif
